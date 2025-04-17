0aeb60c734c2ec48dc763c3206d5e9a2e22b4e58
juanhiginio <juanhiginio06@gmail.com>
2025-04-16 23:17:02 -0500

    Prettier file configuration to avoid syntax errors and code editor crashes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,19 @@
-// @ts-check
-import eslint from '@eslint/js';
-import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
-import globals from 'globals';
+import eslintPluginPrettier from 'eslint-plugin-prettier';
+import prettierConfig from './.prettierrc' assert { type: 'json' };
+import js from '@eslint/js';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
+export default [
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  ...tseslint.configs.strict,
   {
-    ignores: ['eslint.config.mjs'],
-  },
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  eslintPluginPrettierRecommended,
-  {
-    languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-      sourceType: 'commonjs',
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
+    files: ['**/*.ts'],
+    plugins: {
+      prettier: eslintPluginPrettier,
     },
-  },
-  {
     rules: {
-      '@typescript-eslint/no-explicit-any': 'off',
-      '@typescript-eslint/no-floating-promises': 'warn',
-      '@typescript-eslint/no-unsafe-argument': 'warn'
+      'prettier/prettier': ['error', prettierConfig],
     },
   },
-);
+];
